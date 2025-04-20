432d2fffa6efc9532eff29c692a43d992b07c6fa
rushmi0 <gcalku@gmail.com>
2025-04-20 21:37:41 +0700

    new update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,11 +36,48 @@ vim.opt.mouse = ""
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
+-- undo
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- indent
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.indentexpr = "on"
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
 
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+vim.opt.whichwrap:append("<>[]hl")
+
+-- use mouse click but disable when typing
+vim.opt.mouse = "nvch"
+vim.opt.signcolumn = "yes"
+vim.api.nvim_set_option_value("colorcolumn", "80", {})
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = "pum"
+vim.opt.pumblend = 5
+
+
 vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
 vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd([[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  augroup END
+]])
 
 if vim.fn.has("nvim-0.8") == 1 then
 	vim.opt.cmdheight = 0
@@ -56,3 +93,10 @@ vim.filetype.add({
 vim.g.lazyvim_prettier_needs_config = true
 vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_cmp = "blink.cmp"
+vim.g.lazyvim_picker = "snacks"
+
+-- LSP Server to use for Rust.
+-- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
+-- only for diagnostics. The rest of LSP support will still be
+-- provided by rust-analyzer.
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
